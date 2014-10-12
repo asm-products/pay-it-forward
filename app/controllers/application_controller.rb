9f2021fe6d8b2febc05ca3bb56bfee7c853ae4b6
Zane Wolfgang Pickett <Zane.Wolfgang.Pickett@Gmail.com>
2014-10-12 01:13:21 +0000

    bootstrapify devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
-
+  protected
+  
   def ensure_signup_complete
     # Ensure we don't go into an infinite loop
     return if action_name == 'finish_signup'
 
-    # Redirect to the 'finish_signup' page if the user
-    # email hasn't been verified yet
     if current_user && !current_user.email_verified?
       redirect_to finish_signup_path(current_user)
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
